78fd8321f035ae0d4c1c45bbbf6dca6cf903bf7e
Ethan Rahimian <nicootech@gmail.com>
2021-05-27 11:20:59 -0700

    Application Component

---
+++ app/src/main/java/com/nicootech/daggerexcercise/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.nicootech.daggerexcercise;
+
+
+import com.nicootech.daggerexcercise.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/nicootech/daggerexcercise/di/AppComponent.java
@@ -0,0 +1,27 @@
+package com.nicootech.daggerexcercise.di;
+
+import android.app.Application;
+
+import com.nicootech.daggerexcercise.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance// we do not always have to do
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}

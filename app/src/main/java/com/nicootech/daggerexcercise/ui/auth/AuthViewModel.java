72cafeed761738960a1e36cc6e26477321cab15c
Ethan Rahimian <nicootech@gmail.com>
2021-06-02 11:22:42 -0700

    Authentication Network Resource Class (Jetpack best-practice)

--- app/src/main/java/com/nicootech/daggerexcercise/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/nicootech/daggerexcercise/ui/auth/AuthViewModel.java
@@ -15,6 +15,7 @@ import androidx.lifecycle.ViewModel;
 import io.reactivex.Scheduler;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
@@ -23,7 +24,7 @@ public class AuthViewModel extends ViewModel {
 
     private final AuthApi authApi;
 
-    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
 
     @Inject
     public AuthViewModel(AuthApi authApi) {
@@ -33,20 +34,46 @@ public class AuthViewModel extends ViewModel {
     }
 
     public void authenticateWithId(int userId){
-        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+        authUser.setValue(AuthResource.loading((User)null));
+
+        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
+
                 authApi.getUser(userId)
+
+                        // instead of calling onError (error happens)
+                        .onErrorReturn(new Function<Throwable, User>() {
+                            @NonNull
+                            @Override
+                            public User apply(@NonNull Throwable throwable) throws Exception {
+                                User errorUser = new User();
+                                errorUser.setId(-1);
+                                return errorUser;
+                            }
+                        })
+                        .map(new Function<User, AuthResource<User>>() {
+                            @NonNull
+                            @Override
+                            public AuthResource<User> apply(@NonNull User user) throws Exception {
+                                if(user.getId() == -1){
+                                    return AuthResource.error("Could not authenticate", (User)null);
+                                }
+                                // if there is no Error
+                                return AuthResource.authenticated(user);
+                            }
+                        })
+
                 .subscribeOn(Schedulers.io())
         );
 
-        authUser.addSource(source, new Observer<User>() {
+        authUser.addSource(source, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
+            public void onChanged(AuthResource<User> user) {
                 authUser.setValue(user);
                 authUser.removeSource(source);
             }
         });
     }
-    public LiveData<User> observeUser(){
+    public LiveData<AuthResource<User>> observeUser(){
         return authUser;
     }
 

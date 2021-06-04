e26e1f6b8eea6ad2d6772a7fc8a814e51e5004d8
Ethan Rahimian <nicootech@gmail.com>
2021-06-04 14:39:13 -0700

    Scoping SubComponents with Dagger2

--- app/src/main/java/com/nicootech/daggerexcercise/di/ActivityBuildersModule.java
+++ app/src/main/java/com/nicootech/daggerexcercise/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.nicootech.daggerexcercise.di;
 
 import com.nicootech.daggerexcercise.di.auth.AuthModule;
+import com.nicootech.daggerexcercise.di.auth.AuthScope;
 import com.nicootech.daggerexcercise.di.auth.AuthViewModelsModule;
 import com.nicootech.daggerexcercise.di.main.MainFragmentBuilderModule;
 import com.nicootech.daggerexcercise.di.main.MainModule;
+import com.nicootech.daggerexcercise.di.main.MainScope;
 import com.nicootech.daggerexcercise.di.main.MainViewModelModule;
 import com.nicootech.daggerexcercise.ui.auth.AuthActivity;
 import com.nicootech.daggerexcercise.ui.main.MainActivity;
@@ -14,11 +16,14 @@ import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuildersModule {
+
+    @AuthScope
     @ContributesAndroidInjector(
             modules = {AuthViewModelsModule.class, AuthModule.class}
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuilderModule.class, MainViewModelModule.class, MainModule.class}
     )

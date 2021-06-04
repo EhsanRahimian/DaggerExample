f3e94ff8ff974d43bb1b2850756fa28b6b94da4d
Ethan Rahimian <nicootech@gmail.com>
2021-06-04 12:19:49 -0700

    Navigation Drawer AndroidX

--- app/src/main/java/com/nicootech/daggerexcercise/ui/main/MainActivity.java
+++ app/src/main/java/com/nicootech/daggerexcercise/ui/main/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.nicootech.daggerexcercise.BaseActivity;
 import com.nicootech.daggerexcercise.R;
 import com.nicootech.daggerexcercise.ui.main.posts.PostsFragment;
@@ -13,16 +14,23 @@ import com.nicootech.daggerexcercise.ui.main.profile.ProfileFragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
-        //Toast.makeText(this, "This is Main Activity... ", Toast.LENGTH_SHORT).show();
 
         testFragment();
     }
@@ -52,5 +60,27 @@ public class MainActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.nav_profile:{
+
+                break;
+            }
+
+            case R.id.nav_posts:{
+
+                break;
+            }
+
+        }
+
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }

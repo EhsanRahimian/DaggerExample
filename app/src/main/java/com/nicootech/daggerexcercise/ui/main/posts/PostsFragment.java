9641333654b4c18f991246e5a4f5ec4c1aac5300
Ethan Rahimian <nicootech@gmail.com>
2021-06-03 18:49:23 -0700

    RxJava Query in ViewModel with Subcomponent Dependency

--- app/src/main/java/com/nicootech/daggerexcercise/ui/main/posts/PostsFragment.java
+++ app/src/main/java/com/nicootech/daggerexcercise/ui/main/posts/PostsFragment.java
@@ -1,17 +1,23 @@
 package com.nicootech.daggerexcercise.ui.main.posts;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nicootech.daggerexcercise.R;
+import com.nicootech.daggerexcercise.models.Post;
+import com.nicootech.daggerexcercise.ui.main.Resource;
 import com.nicootech.daggerexcercise.viewmodels.ViewModelProviderFactory;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,6 +46,20 @@ public class PostsFragment extends DaggerFragment {
         //recyclerView = view.findViewById(R.id.recycler_view);
 
         viewModel = new ViewModelProvider(this, providerFactory).get(PostsViewModel.class);
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+        viewModel.observePosts().removeObservers(getViewLifecycleOwner());
+        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
+            @Override
+            public void onChanged(Resource<List<Post>> listResource) {
+                if(listResource != null){
+                    Log.d(TAG, "onChanged: " + listResource.data);
+                }
+            }
+        });
     }
 
 
